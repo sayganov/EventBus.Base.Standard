935028a7ac9dbd3c224b8fb62384db4b33fd16f6
Georgy Sayganov <georgysay@gmail.com>
2020-01-21 17:58:18 +0300

    Add the Handling class.

---
+++ src/EventBus.Base.Standard/Configuration/Handling.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventBus.Base.Standard.Configuration
+{
+    public static class Handling
+    {
+        public static IServiceCollection AddEventBusHandling<T>(this IServiceCollection services, IEnumerable<T> handlers)
+        {
+            foreach (var handler in handlers)
+            {
+                services.AddTransient(handler.GetType());
+            }
+
+            return services;
+        }
+    }
+}
